9e035001a50096d6c16581fa0b47bfaff277758a
Ajwad <shaikhajwad10@gmail.com>
2018-05-29 01:24:50 +0530

    SRISTI Sanshodh Collect Init

--- collect_app/src/main/java/org/odk/collect/android/activities/MainMenuActivity.java
+++ collect_app/src/main/java/org/odk/collect/android/activities/MainMenuActivity.java
@@ -239,9 +239,9 @@ public class MainMenuActivity extends CollectAbstractActivity {
 
         {
             // dynamically construct the "ODK Collect vA.B" string
-            TextView mainMenuMessageLabel = findViewById(R.id.main_menu_header);
-            mainMenuMessageLabel.setText(Collect.getInstance()
-                    .getVersionedAppName());
+            //TextView mainMenuMessageLabel = findViewById(R.id.main_menu_header);
+            //mainMenuMessageLabel.setText(Collect.getInstance()
+                    //.getVersionedAppName());
         }
 
         File f = new File(Collect.ODK_ROOT + "/collect.settings");
